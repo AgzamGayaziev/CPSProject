807f575d14a406c891e3a6b931f24fce0cc297b5
SpeedWagon <Meloncholy.Shiro@yandex.com>
2020-07-20 15:34:37 +0300

    Версия: 1.1.0: Добавлен Слайдер!

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,43 @@
-import Swiper from "swiper";
-import "swiper/swiper-bundle.css";
+import Swiper, { Pagination } from "swiper";
+Swiper.use(Pagination);
 
-const menuListener = (() => {
+const menuController = (() => {
     const wrapper = document.querySelector(".wrapper");
     const menu = wrapper.querySelector(".menu");
     const openButton = wrapper.querySelector("#openMenu");
-    const closeButton = wrapper.querySelector("#closeMenu");
+    const closeButton = wrapper.querySelector("#closeMenu *");
 
-    openButton.addEventListener("click", () => {
-        menu.classList.add("menu--active");
-        wrapper.classList.add("wrapper--disabled");
-    });
+    function menuOpenHandler() {
+        openButton.addEventListener(
+            "click",
+            (e) => {
+                e.preventDefault();
+                menu.classList.add("menu--active");
+                wrapper.classList.add("wrapper--disabled");
+                awaitClose();
+            },
+            { once: true }
+        );
+    }
 
-    closeButton.addEventListener("click", () => {
-        menu.classList.remove("menu--active");
-        wrapper.classList.remove("wrapper--disabled");
-    });
+    function menuCloseHandler(e) {
+        e.preventDefault();
+        if (e.keyCode === 27 || e.target === wrapper || e.target === closeButton) {
+            window.removeEventListener("keydown", menuCloseHandler);
+            window.addEventListener("click", menuCloseHandler);
+            menu.classList.remove("menu--active");
+            wrapper.classList.remove("wrapper--disabled");
+
+            menuOpenHandler();
+        }
+    }
+
+    function awaitClose() {
+        window.addEventListener("keydown", menuCloseHandler);
+        window.addEventListener("click", menuCloseHandler);
+    }
+
+    menuOpenHandler();
 })();
 
 const initSwiper = (() => {
@@ -25,21 +47,34 @@ const initSwiper = (() => {
 
     const breakpointChecker = () => {
         if (breakpoint.matches === true) {
-            if (mySwiper !== undefined) mySwiper.destroy(true, true);
-            return;
+            if (mySwiper !== undefined) {
+                mySwiper.destroy(true, true);
+            } else {
+                return;
+            }
         } else if (breakpoint.matches === false) {
             return enableSwiper();
         }
     };
 
     const enableSwiper = function () {
-        mySwiper = new Swiper(".swiper-container", {
-            slidesPerView: "1",
+        mySwiper = new Swiper(".swiper", {
+            slidesPerView: "auto",
             loop: false,
             a11y: true,
+            spaceBetween: 16,
+            slideClass: "swiper__slide",
+            slideActiveClass: "swiper__slide--active",
+            wrapperClass: "swiper__wrapper",
+            containerModifierClass: "swiper--",
+            slideNextClass: "swiper__slide--next",
+            slidePrevClass: "swiper__slide--prev",
             pagination: {
-                el: ".swiper-pagination",
+                el: ".swiper__pagination",
                 clickable: false,
+                modifierClass: "swiper__pagination--",
+                bulletClass: "swiper__bullet",
+                bulletActiveClass: "swiper__bullet--active",
             },
         });
     };
